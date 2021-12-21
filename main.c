07b53b5cc228623c49644559b5ac7f1a9185242f
druckhead <57668763+druckhead@users.noreply.github.com>
2021-12-21 22:05:58 +0200

    initial commit

---
+++ main.c
@@ -0,0 +1,10 @@
+/**
+ * @file main.c
+ * @author daniel raz (https://github.com/druckhead)
+ * @brief 
+ * @version 0.1
+ * @date 2021-12-21
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
---
+++ nodes.c
@@ -0,0 +1,10 @@
+/**
+ * @file nodes.c
+ * @author daniel raz (https://github.com/druckhead)
+ * @brief 
+ * @version 0.1
+ * @date 2021-12-21
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
