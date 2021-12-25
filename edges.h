37205f2f312f559cb936c10cc2d5a318ed6fa0f7
Daniel Raz <daniel32raz@gmail.com>
2021-12-25 20:35:20 +0200

    very basic definition

--- edges.h
+++ edges.h
@@ -1,10 +1,23 @@
 /**
  * @file edges.h
  * @author daniel raz (https://github.com/druckhead)
- * @brief 
+ * @brief
  * @version 0.1
  * @date 2021-12-21
- * 
+ *
  * @copyright Copyright (c) 2021
- * 
+ *
  */
+
+#ifndef __EDGE_H__
+
+#define __EDGE_H__
+
+typedef struct Edge
+{
+    int weight;
+    int src;
+    int dest;
+} Edge;
+
+#endif
--- nodes.h
+++ nodes.h
@@ -1,10 +1,22 @@
 /**
  * @file nodes.h
  * @author daniel raz (https://github.com/druckhead)
- * @brief 
+ * @brief
  * @version 0.1
  * @date 2021-12-21
- * 
+ *
  * @copyright Copyright (c) 2021
- * 
+ *
  */
+
+#ifndef __NODE_H__
+
+#define __NODE_H__
+
+typedef struct Node
+{
+    int id;
+    
+} Node;
+
+#endif
