911fcb706ae64c8034bbdbfbec756f889c68e5fc
Daniel Raz <daniel32raz@gmail.com>
2021-12-28 15:56:47 +0200

    initial function def

---
+++ graph.c
@@ -0,0 +1,37 @@
+/**
+ * @file graph.c
+ * @author your name (you@domain.com)
+ * @brief
+ * @version 0.1
+ * @date 2021-12-28
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
+
+#include "graph.h"
+#include "nodes.h"
+#include "edges.h"
+
+void graph_init(p_node *head)
+{
+    
+}
+void del_init_graph(p_node *head)
+{
+
+}
+
+void add_node(p_node *head)
+{
+}
+void del_node(p_node *head)
+{
+}
+
+void shortest_path(p_node *head)
+{
+}
+void tsp(p_node *head)
+{
+}
